63cbb84aaa212adf05ded82a92f32188c6a75a5a
AlexMiroshnikov <ilex>
2016-04-09 10:57:11 +0700

    Init tests

---
+++ tests/Tokenizer/FactoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mufuphlex\Tests\Textonic\Tokenizer;
+
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMake()
+    {
+        
+    }
+}
---
+++ tests/Tokenizer/TokenizerDefaultTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mufuphlex\Tests\Textonic\Tokenizer;
+
+class TokenizerDefaultTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTokenize()
+    {
+        
+    }
+}
